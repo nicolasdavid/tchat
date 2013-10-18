ebc43a5929fd15757a37d0a5eb118f0f19be5282
Nicolas DAVID <nicolas.david@eleves.ec-nantes.fr>
2013-10-18 15:11:39 +0200

    add attributes, constructor for server and the main method of the interface Function --> need to implement methods in Server.class

---
+++ tchat/src/interfaces/Function.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author Nicolas
+ */
+public interface Function {
+    
+    /**
+     * method to connect user to the server "Call me daddy"
+     * @param id
+     * @return 
+     */
+    public String request(String id);
+    
+    public String getMessage();
+    
+}
--- tchat/src/serveur/Serveur.java
+++ tchat/src/serveur/Serveur.java
@@ -4,10 +4,41 @@
  */
 package serveur;
 
+import interfaces.Function;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
- * @author Nicolas
+ * @author Nicolas & Sylvain
  */
-public class Serveur {
+public class Serveur implements Function{
+    
+    protected String name ;
+    protected ArrayList<Thread> listThreads;
+    protected ArrayList<String> listMessages;
+    protected HashMap<String,Integer> listeCorrespondance; //the "String" represents the last message received by the user number "Integer"
+    
+    public Serveur(){
+      this.name="Call me daddy";
+      this.listThreads = new ArrayList<Thread>();
+      this.listMessages = new ArrayList<String>();
+      this.listeCorrespondance = new HashMap<String,Integer>();
+    }
+    
+    public static void main(String[] args){
+        Serveur serv = new Serveur();
+    }
+
+    @Override
+    public String request(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
