c997f2d1115a5bd91b5983e1ddaf5425d3b91dee
Nicolas DAVID <nicolas.david@eleves.ec-nantes.fr>
2013-10-20 14:00:13 +0200

    creating main methods of client - need to add the rmi

--- tchat/src/client/Client.java
+++ tchat/src/client/Client.java
@@ -4,11 +4,30 @@
  */
 package client;
 
+import interfaces.Function;
+import java.util.Scanner;
+
 /**
  *
  * @author Nicolas
  */
 public class Client {
+       protected String id;
+       protected Function f;
        
-
+       public Client(){
+           Scanner sc = new Scanner(System.in);
+           System.out.println("Please enter your name to enter the tchat");
+           this.id = sc.nextLine();
+           try{
+               System.out.println(f.request(this.id));
+           }
+           catch(Exception e){
+               System.out.println(e.getMessage());
+           }
+       }
+       
+       public static void main(String[] args){
+             Client user = new Client();  
+       }
 }
